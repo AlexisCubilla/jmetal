7a89da045902b2206713bb15836afadc95f923f0
Nicholas Jara <nicholasjara1998@gmail.com>
2024-06-19 19:52:57 -0400

    chore: Add CustomObserver class for progress tracking

---
+++ calibration/observer.py
@@ -0,0 +1,72 @@
+
+from jmetal.util.observer import Observer
+from tqdm import tqdm
+class CustomObserver(Observer):
+    def __init__(self):
+        self.progress_bar = None
+        self.progress = 0
+        self._max = 0
+        self._porcetual_progress=0
+        self._elapsed=0
+        self._remaining=0
+        self._elapsed_str=""
+        self._remaining_str=""
+        self._solutions=None
+
+
+        
+    def update(self, *args, **kwargs):
+        evaluations = kwargs["EVALUATIONS"]
+        # solutions=kwargs["SOLUTIONS"]
+        # if solutions:
+        #     var = []
+        #     func= []
+        #     for i in solutions[0].variables:
+        #         if(isinstance(i.variables[0], int)):
+        #             var+=i.variables
+        #         elif(isinstance(i.variables[0], float)):
+        #             var+=i.variables
+        #         else:
+        #             var+=i.variables[0]
+            
+        #     for i in solutions[0].objectives:
+        #         func.append(i)
+
+        #     self._solutions = {"variables":var, "objectives":func}      
+        if not self.progress_bar:
+            self.progress_bar = tqdm(total=self._max, ascii=True, desc="Progress")
+
+        self.progress_bar.update(evaluations - self.progress)
+        self.progress = evaluations
+
+        if self.progress >= self._max:
+            self.progress_bar.close()
+
+        #enviar un valor de 0 a 100 que represente el progreso de la optimización
+        self._porcetual_progress= int((evaluations/self._max)*100)
+
+        elapsed= self.progress_bar.format_dict["elapsed"]
+        total= self.progress_bar.format_dict["total"]
+        n= self.progress_bar.format_dict["n"]
+        rate = self.progress_bar.format_dict["rate"]
+        remaining = (total - n) / rate if rate and self.progress_bar.total else 0
+
+        self._remaining_str = self.progress_bar.format_interval(remaining)
+        self._elapsed_str = self.progress_bar.format_interval(elapsed)
+    @property
+    def porcetual_progress(self):
+        return self._porcetual_progress
+
+    @property
+    def elapsed(self):
+        return self._elapsed_str
+    
+    @property
+    def remaining(self):
+        return self._remaining_str
+    @property
+    def solutions(self):
+        return self._solutions
+    
+    def set_max(self, max:int):
+        self._max = max
