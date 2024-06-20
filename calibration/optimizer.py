3cdc853ee4d1fb2ea539fd403870d94a7587c37f
Nicholas Jara <nicholasjara1998@gmail.com>
2024-06-20 03:28:35 -0400

    Refactor CalibrationData class to include parent attribute in inputs

--- calibration/optimizer.py
+++ calibration/optimizer.py
@@ -1,13 +1,24 @@
 import json
 from jmetal.algorithm.multiobjective.nsgaii import NSGAII
-from jmetal.operator import  IntegerPolynomialMutation, PolynomialMutation, SBXCrossover, BitFlipMutation, SimpleRandomMutation, UniformMutation
+from jmetal.operator import (
+    IntegerPolynomialMutation,
+    PolynomialMutation,
+    SBXCrossover,
+    BitFlipMutation,
+    SimpleRandomMutation,
+    UniformMutation,
+)
 from jmetal.operator.mutation import CompositeMutation
 from jmetal.util.termination_criterion import StoppingByEvaluations
 import numpy as np
 from calibration.data import CalibrationData
 from calibration.problem import CalibrationProblem
-from calibration.termination_criterion import  StopByEvaluationWithRestrictions
-from jmetal.operator.crossover import CompositeCrossover, IntegerSBXCrossover, SPXCrossover
+from calibration.termination_criterion import StopByEvaluationWithRestrictions
+from jmetal.operator.crossover import (
+    CompositeCrossover,
+    IntegerSBXCrossover,
+    SPXCrossover,
+)
 from data import Data
 from jmetal.util.observer import ProgressBarObserver
 from jmetal.util.constraint_handling import is_feasible
@@ -15,10 +26,11 @@ from jmetal.core.solution import CompositeSolution
 from jmetal.core.quality_indicator import FitnessValue
 from typing import List
 
+
 class OptimizerWithCalibration:
     def __init__(self, websocket):
         self.websocket = websocket
-    
+
     def optimize(self, data_recieved):
         try:
             self.data = CalibrationData(data_recieved)
@@ -32,79 +44,74 @@ class OptimizerWithCalibration:
             solutions = self.run_nsgaii()
 
             if solutions:
-                variables= self.process_results(solutions, self.data)
-                
+                variables = self.process_results(solutions, self.data)
+
             return self.buildMessage(variables), None
         except Exception as e:
             return None, str(e)
 
-        
-    def buildMessage(self, variables:List[float]):
+    def buildMessage(self, variables: List[float]):
         message = {
             "type": "close",
-            "data": [
-                {
-                    "id": input["id"],
-                    "parent": input["parent"],
-                    "data": variable
-                }
-                for input, variable in zip(self.data.inputs, variables)
-            ]
+            "data": {
+                "inputs": [
+                    {"id": input["id"], "parent": input["parent"], "data": variable}
+                    for input, variable in zip(self.data.inputs, variables)
+                ]
+            },
         }
         return message
-        
+
     def mutation(self):
         mapped_mutation_functions = {
-                        "IntegerPolynomialMutation": IntegerPolynomialMutation,
-                        "PolynomialMutation": PolynomialMutation,
-                        "SimpleRandomMutation": SimpleRandomMutation,
-                        "UniformMutation": UniformMutation,
-                        "BitFlipMutation": BitFlipMutation
+            "IntegerPolynomialMutation": IntegerPolynomialMutation,
+            "PolynomialMutation": PolynomialMutation,
+            "SimpleRandomMutation": SimpleRandomMutation,
+            "UniformMutation": UniformMutation,
+            "BitFlipMutation": BitFlipMutation,
         }
-        mutation_list=[]
+        mutation_list = []
         for mutation in self.mutations:
             mutation_type = mutation.name
-            probability =   mutation.probability
+            probability = mutation.probability
             distribution_index = mutation.distribution_index
-            perturbation = None #mutation.get("perturbation")
+            perturbation = None  # mutation.get("perturbation")
             if probability is not None:
                 if distribution_index is not None:
                     mutation_kwargs = {
                         "probability": probability,
-                        "distribution_index": distribution_index
+                        "distribution_index": distribution_index,
                     }
                 elif perturbation is not None:
                     mutation_kwargs = {
                         "probability": probability,
-                        "perturbation": perturbation
+                        "perturbation": perturbation,
                     }
                 else:
-                    mutation_kwargs = {
-                        "probability": probability
-                    }
-                mutation_list.append(mapped_mutation_functions[mutation_type](**mutation_kwargs))
+                    mutation_kwargs = {"probability": probability}
+                mutation_list.append(
+                    mapped_mutation_functions[mutation_type](**mutation_kwargs)
+                )
         return mutation_list
-
 
     def crossover(self):
         mapped_crossover_functions = {
             "IntegerSBXCrossover": IntegerSBXCrossover,
             "SBXCrossover": SBXCrossover,
-            "SPXCrossover": SPXCrossover
+            "SPXCrossover": SPXCrossover,
         }
         crossover_list = []
         for crossover in self.crossovers:
             crossover_type = crossover.name
             probability = crossover.probability
             distribution_index = crossover.distribution_index
-            crossover_kwargs = {
-                "probability": probability
-            }
+            crossover_kwargs = {"probability": probability}
             if distribution_index is not None:
                 crossover_kwargs["distribution_index"] = distribution_index
-            crossover_list.append(mapped_crossover_functions[crossover_type](**crossover_kwargs))
+            crossover_list.append(
+                mapped_crossover_functions[crossover_type](**crossover_kwargs)
+            )
         return crossover_list
-
 
     def run_nsgaii(self):
         algorithm = NSGAII(
@@ -113,11 +120,12 @@ class OptimizerWithCalibration:
             offspring_population_size=self.offspring_population_size,
             mutation=CompositeMutation(self.mutation()),
             crossover=CompositeCrossover(self.crossover()),
-        # termination_criterion=StoppingByEvaluations(max_evaluations=self.max_evaluations),
-            termination_criterion=StopByEvaluationWithRestrictions( 
-                                                                     max_evaluations=self.max_evaluations,
-                                                                     extra_evaluations=self.extra_evaluations,
-                                                                     check_feasibility=True),
+            # termination_criterion=StoppingByEvaluations(max_evaluations=self.max_evaluations),
+            termination_criterion=StopByEvaluationWithRestrictions(
+                max_evaluations=self.max_evaluations,
+                extra_evaluations=self.extra_evaluations,
+                check_feasibility=True,
+            ),
         )
         observer = ProgressBarObserver(self.max_evaluations)
         algorithm.observable.register(observer)
@@ -125,19 +133,23 @@ class OptimizerWithCalibration:
         solutions = algorithm.get_result()
         return solutions
 
-    def process_results(self, solutions:  List[CompositeSolution], data:Data):
+    def process_results(self, solutions: List[CompositeSolution], data: Data):
         min_fitness: float = float("inf")
         final_solution_variables: [] = []
-        if True: #if check_feasibility:
-            list_of_composite_solutions: List[CompositeSolution] = [solution for solution in solutions if is_feasible(solution)]
+        if True:  # if check_feasibility:
+            list_of_composite_solutions: List[CompositeSolution] = [
+                solution for solution in solutions if is_feasible(solution)
+            ]
             if list_of_composite_solutions:
                 for i, composite_solution in enumerate(list_of_composite_solutions):
-                    if(composite_solution.variables[0].objectives[0] < min_fitness):
+                    if composite_solution.variables[0].objectives[0] < min_fitness:
                         min_fitness = composite_solution.variables[0].objectives[0]
-                        final_solution_variables = composite_solution.variables[0].variables
-                    
+                        final_solution_variables = composite_solution.variables[
+                            0
+                        ].variables
+
         return final_solution_variables
-    
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     pass
